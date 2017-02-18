7703b2bdaf5418bfc2c22d2c36af11c3b8cfaea6
Neokoiaigone <louisalexandrebricq@yahoo.fr>
2017-02-18 01:09:22 +0100

    header ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+
+    </header>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,9 +54,14 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
                         @if (Auth::guest())
+                            <li><a href="{{ route('article.index') }}">Article</a></li>
+                            <li><a href="{{ route('form.index') }}">Contact</a></li>
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('article.create') }}">Publication</a></li>
+                            <li><a href="{{ route('article.index') }}">Article</a></li>
+                            <li><a href="{{ route('form.index') }}">Contact</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
