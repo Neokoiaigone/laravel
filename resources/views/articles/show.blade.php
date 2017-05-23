82d349edb2650db9fbdd3cc267b58942148e2441
Unknown <greenwaets@gmail.com>
2017-05-23 15:19:18 +0200

    Changed article show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,7 +4,7 @@
     <header class="intro-header" style="background-image: url('/img/home-bg.jpg')">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="col-lg-8 col-lg-offset-2 col-md-12 col-md-offset-1">
                     <div class="site-heading">
                         <h1>{{ $article->title }}</h1>
 
