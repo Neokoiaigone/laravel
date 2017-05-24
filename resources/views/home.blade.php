29529b28d6bf57e89c0d04db96c75bc5e332e415
Unknown <greenwaets@gmail.com>
2017-05-25 00:17:39 +0200

    fixed home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="site-heading">
-                        <h1>Dashboard</h1>
-                        <hr class="small">
-                        <span class="subheading">You are logged in!</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <header class="intro-header">
     </header>
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Dashboard</div>
-
                     <div class="panel-body">
-                        You are logged in!
+                        Vous êtes bien connectés!
                     </div>
                 </div>
             </div>
