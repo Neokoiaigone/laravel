b471ea24ff489c6a3b25cffbcefdb594081acd6d
Unknown <greenwaets@gmail.com>
2017-05-27 01:41:01 +0200

    started designing a little bit communauté

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,25 +5,30 @@
     <!-- Navigation -->
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" ></header>
+    <header class="intro-header"></header>
 
     <!-- Main Content -->
     <div class="container">
         <div class="row">
-            <div class="col-md-offset-1 col-md-10 ">
+            <div class="col-md-12 ">
                 <div class="post-preview">
-                    <h2>La communauté</h2>
+                    <h2 style="font-size: 3em;">La communauté :</h2>
                     @include('messages.success')
                     @include('messages.error')
                     @foreach($users as $user)
+
                         <a href="{{ route('user.show', $user->id) }}">
                             <div class="post-title z-depth-4 col l4 m6 s12">
-                                <h2>
-                                    {{$user->name}}
-                                </h2>
+                                <div class="boxprofil">
+                                    <h3>
+                                        {{$user->name}}
+                                    </h3>
+                                    <h5>Compte créé le : {{ $user->created_at }}
+                                        Nombre d'article créé :
+                                    </h5>
+                                </div>
                             </div>
                         </a>
-
                     @endforeach
                 </div>
                 <hr>
